858757da42a37f4f63faaa8f835b5a9157663581
Tiago C. Brandão <tiago_curvelo@hotmail.com>
2021-01-14 16:01:16 -0300

    inicio do projeto em .Net Core

---
+++ README.md
@@ -0,0 +1,3 @@
+# Curso DIO - Configuração da arquitetura back-end com .NET Core
+
+Neste curso aprendemos de forma prática a desenvolver um projeto com .NET Core usando RESTful APIs e outras funções que otimizam o trabalho com a tecnologia.
---
+++ curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,48 @@
+﻿using curso.api.Filters;
+using curso.api.Models;
+using curso.api.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        /// <summary>
+        /// teste
+        /// </summary>
+        /// <param name="loginViewModelInput"></param>
+        /// <returns></returns>
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", type: typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", type: typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro interno", type: typeof(ErroGenericoViewModel))]
+        [HttpPost]
+        [Route("logar")]
+        [ValidacaoModelStateCustomizado]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(new ValidaCampoViewModelOutput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
+            //}
+
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        [ValidacaoModelStateCustomizado]
+        public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
+        {
+            return Created("", registroViewModelInput);
+        }
+
+    }
+}
